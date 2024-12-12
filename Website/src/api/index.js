32ff9405df3d17048ffc17af52bac6fd71d0a2a6
anugerah <22081010160@student.upnjatim.ac.id>
2024-12-12 13:04:04 +0700

    Finalize

--- Website/src/api/index.js
+++ Website/src/api/index.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
-// const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
-const API_BASE_URL = "http://localhost:4000";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+// const API_BASE_URL = "http://localhost:4000";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
 });
 
-// Tambahkan JWT token ke setiap request jika tersedia
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
